7ff4fe64717c15d62c03e11a876bb6bf5e549eb2
Eliot Garcia <eag0828@gmail.com>
2018-03-02 08:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2;
   return n + two
 
-  // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
